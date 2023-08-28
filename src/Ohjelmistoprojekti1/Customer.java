67bb039097ab32b3b035b5153d728719dd94357c
nasut <zounena1234@gmail.com>
2023-08-28 23:07:18 +0300

    Exercises

---
+++ src/Ohjelmistoprojekti1/Customer.java
@@ -0,0 +1,16 @@
+package Ohjelmistoprojekti1;
+
+public class Customer {
+    int id;
+    long start_time;
+    long end_time;
+
+    Customer(int id, long start_time, long end_time) {
+        this.id = id;
+        this.start_time = start_time;
+        this.end_time = end_time;
+    }
+
+
+
+}
---
+++ src/Ohjelmistoprojekti1/Main.java
@@ -0,0 +1,8 @@
+package Ohjelmistoprojekti1;
+
+public class Main {
+
+    Customer customer = new Customer(1,5,10);
+
+
+}
