f8c827e15ac8a49127166c259b93cc0f7acbf5bc
Nasim Zouneibiri <111856757+nasudesu@users.noreply.github.com>
2023-10-04 11:24:02 +0300

    7.4

---
+++ src/Chapter7/Exercises4/currency.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS currency;
+CREATE DATABASE currency;
+USE currency;
+
+CREATE TABLE IF NOT EXISTS CurrencyConvert
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    currency VARCHAR(3)     NOT NULL,
+    rate     DECIMAL(10, 4) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO CurrencyConvert (currency, rate)
+VALUES ('USD', 1.00),
+       ('EUR', 0.95),
+       ('JPY', 149.68),
+       ('GBP', 0.83),
+       ('CNY', 7.21),
+       ('AUD', 1.58),
+       ('CAD', 1.37),
+       ('CHF', 0.92);
+
+DROP USER IF EXISTS 'appuser'@'localhost';
+CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE ON CURRENCY.* TO 'appuser'@'localhost';
+
+
+
+
+
+
---
+++ src/Chapter7/Exercises4/entity/CurrencyConverGUI.java
@@ -0,0 +1,85 @@
+package Chapter7.Exercises4.entity;
+
+import Chapter7.Exercises4.application.CCcontrol;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class CurrencyConverGUI extends Application {
+    CCcontrol cCcontrol;
+
+    public void init() {
+        cCcontrol = new CCcontrol(this);
+    }
+
+    public void start(Stage stage) {
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+
+        Button convert = new Button("Convert");
+        TextField amountToConvert = new TextField("");
+        TextField convertedAmount = new TextField("");
+
+        convert.setOnAction(event -> {
+            try {
+                double value = Double.parseDouble(amountToConvert.getText());
+                cCcontrol.setValue(value);
+                convertedAmount.setText(String.valueOf(cCcontrol.convertValue()));
+            } catch (Exception e) {
+                amountToConvert.setText("Give a number");
+                System.out.println("Give a number");
+            }
+        });
+
+        ChoiceBox<String> choiceBox1 = new ChoiceBox<>();
+        choiceBox1.setItems(FXCollections.observableArrayList("EUR", "USD", "GBP", "JPY", "CNY", "CAD", "CHF", "AUD"));
+        choiceBox1.setOnAction(event -> {
+            String selectedOption = choiceBox1.getValue();
+            cCcontrol.setType1(selectedOption);
+        });
+
+        ChoiceBox<String> choiceBox2 = new ChoiceBox<>();
+        choiceBox2.setItems(FXCollections.observableArrayList("EUR", "USD", "GBP", "JPY", "CNY", "CAD", "CHF", "AUD"));
+        choiceBox2.setOnAction(event -> {
+            String selectedOption = choiceBox2.getValue();
+            cCcontrol.setType2(selectedOption);
+        });
+
+        HBox hBox = new HBox();
+        hBox.getChildren().add(choiceBox1);
+        hBox.getChildren().add(choiceBox2);
+
+        Label label1 = new Label("Convert amount");
+        Label label2 = new Label("Converted amount");
+        Label label3 = new Label("Chose currency from right from what u want to convert and from\n left to what u want to convert to enter values then press convert");
+
+
+        gridPane.add(label1, 0, 0);
+        gridPane.add(amountToConvert, 0, 1);
+
+        GridPane.setHalignment(convert, HPos.CENTER);
+        gridPane.add(convert, 1, 0);
+        gridPane.add(hBox, 1, 1);
+
+        gridPane.add(label2, 2, 0);
+        gridPane.add(convertedAmount, 2, 1);
+
+        GridPane.setHalignment(label3, HPos.CENTER);
+        gridPane.add(label3, 0, 2, 3, 1);
+
+        Scene view = new Scene(gridPane, 500, 200);
+        view.getStylesheets().add("CSS.css");
+        stage.setTitle("Currency convert");
+        stage.setScene(view);
+        stage.show();
+    }
+}
